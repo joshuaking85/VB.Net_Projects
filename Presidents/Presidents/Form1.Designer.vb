1f8ee4386abfe36e227a9e7f0efdd65104696e58
Joshua King <joshuaking85@gmail.com>
2015-10-02 14:33:04 -0400

    Add Presidents

---
+++ Presidents/Presidents/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.dgvOutput = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(12, 297)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(273, 48)
+        Me.btnSort.TabIndex = 0
+        Me.btnSort.Text = "Sort the file Age.txt"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'dgvOutput
+        '
+        Me.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOutput.Location = New System.Drawing.Point(12, 12)
+        Me.dgvOutput.Name = "dgvOutput"
+        Me.dgvOutput.Size = New System.Drawing.Size(584, 258)
+        Me.dgvOutput.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(362, 297)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(219, 48)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(617, 361)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dgvOutput)
+        Me.Controls.Add(Me.btnSort)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvOutput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSort As System.Windows.Forms.Button
+    Friend WithEvents dgvOutput As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Presidents/Presidents/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+
+        'sort the file Age.txt (or text) by ages...
+        Dim agestAtInaug() As String = IO.File.ReadAllLines("Age.txt")
+        Dim query = From line In agestAtInaug
+                    Let name = line.Split(","c)(0)
+                    Let age = CInt(line.Split(","c)(1))
+                    Order By age
+                    Select name, age
+        dgvOutput.DataSource = query.ToList
+        dgvOutput.CurrentCell = Nothing
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'sort the file Age.txt (or text) by ages...
+        Dim agestAtInaug() As String = IO.File.ReadAllLines("Age.txt")
+        Dim query = From line In agestAtInaug
+                    Let name = line.Split(","c)(0)
+                    Let age = CInt(line.Split(","c)(1))
+                    Order By age
+                    Select line
+        IO.File.WriteAllLines("Sorted.csv", query)
+    End Sub
+End Class
