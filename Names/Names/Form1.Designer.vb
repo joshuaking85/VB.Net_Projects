4a2ed518804f3b8c2dde2401ce13dd0c4f6b5251
Joshua King <joshuaking85@gmail.com>
2015-10-02 14:32:41 -0400

    Add Names

---
+++ Names/Names/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbName = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnQuery = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'tbName
+        '
+        Me.tbName.Location = New System.Drawing.Point(102, 19)
+        Me.tbName.Name = "tbName"
+        Me.tbName.Size = New System.Drawing.Size(188, 20)
+        Me.tbName.TabIndex = 1
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(28, 45)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(262, 57)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add Person To File"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnQuery
+        '
+        Me.btnQuery.Location = New System.Drawing.Point(28, 108)
+        Me.btnQuery.Name = "btnQuery"
+        Me.btnQuery.Size = New System.Drawing.Size(262, 56)
+        Me.btnQuery.TabIndex = 3
+        Me.btnQuery.Text = "Determine If Person is in File"
+        Me.btnQuery.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(28, 170)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(262, 60)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Delete Person from file"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 342)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnQuery)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.tbName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tbName As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnQuery As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+
+End Class
---
+++ Names/Names/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.IO  'Import system library
+
+Public Class Form1
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        'Add to file
+        Dim person As String = tbName.Text
+        If person <> "" Then
+            If IsInFile(person) Then
+                MessageBox.Show(person & " is already in file.", " Alert")
+            Else
+                Dim sw As StreamWriter = File.AppendText("Names.txt")
+                sw.WriteLine(person)
+                sw.Close()
+                MessageBox.Show(person & " added to file.", "Name Added")
+                tbName.Clear()
+                tbName.Focus()
+            End If
+        Else
+            MessageBox.Show("You must enter a name.", "Info Incomplete!")
+        End If
+    End Sub
+
+    Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
+        'determine if person is in file
+        Dim person As String = tbName.Text
+        If person <> "" Then
+            If IsInFile(person) Then
+                MessageBox.Show(person & " is in the file.", "YES")
+            Else
+                MessageBox.Show(person & " is NOT in the file.", "NO")
+            End If
+        Else
+            MessageBox.Show("You must enter a name.", "Info Incomplete")
+        End If
+        tbName.Clear()
+        tbName.Focus()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        'remove a person from the list
+        Dim person As String = tbName.Text
+        If person <> "" Then
+            If IsInFile(person) Then
+                'code the remove process
+                Dim sr As StreamReader = File.OpenText("Names.txt")
+                Dim sw As StreamWriter = File.CreateText("temp.txt")
+                Dim individual As String
+                Do Until sr.EndOfStream
+                    individual = sr.ReadLine
+                    If individual <> person Then
+                        sw.WriteLine(individual)
+                    End If
+                Loop
+                sr.Close()
+                sw.Close()
+                File.Delete("Names.txt")
+                File.Move("temp.txt", "Names.txt")
+                MessageBox.Show(person & " removed from file.", "Name Removed")
+            Else
+                MessageBox.Show(person & " is not in the file.", "Name Not Found")
+            End If
+        Else
+            MessageBox.Show("You must enter a name.", "Info Incomplete")
+        End If
+        tbName.Clear()
+        tbName.Focus()
+    End Sub
+
+    Function IsInFile(person As String) As Boolean
+        'Determine if person is in file named names.txt
+        If File.Exists("Names.txt") Then
+            Dim sr As StreamReader = File.OpenText("Names.txt")
+            Dim individual As String
+            Do Until sr.EndOfStream
+                individual = sr.ReadLine
+                If individual = person Then
+                    sr.Close()
+                    Return True
+                End If
+            Loop
+            sr.Close()
+        End If
+        Return False
+    End Function
+End Class
